b57650cac4c9f272ef5547d6cdaaa8498f1029d8
hexie1995 <hexie1995@g.ucla.edu>
2023-06-11 19:35:12 -0400

    updated the vectorized version

--- code/src/em.py
+++ code/src/em.py
@@ -12,13 +12,15 @@ class EM():
         Start with the non-gauranteed version and move on. 
         """
         self.mode = mode
+        self.H = None
         
         
     def fit(self, H):
         self.e_vec, self.H_vec = H.e_vecs, H.H_vecs
+        self.H = H
         
         
-    def par(self, parameters = None, max_iter = 100)
+    def par(self, parameters = None, max_iter = 100):
     
         #initial guess, if not given then start with a default guess.
         if parameters:
@@ -26,77 +28,45 @@ class EM():
         else:   
             heta, hgamma, hbeta = 0.1, 0.05, 3
         
-        H_vec= self.H_vec
-        e_vec= self.e_vec
         
-        total_edge = H_vec[-1].num_edges
         
-        #intialize error term
-        true_error = 1
+        # in the original code this measn there's only three steps, because intial hypergraph is empty and thus ignored
+        # now this is to say that there should be at least one hyper edge exists in order for the model to work
+        # at this step the ommitnig first Hyper graph is already done
+        node_count = self.H.node_count_list[1:] 
+        edge_count = self.H.edge_count_list[1:] 
+        # each of the column here in this matrix is an element in the k_vec list
+        # IX is the Nmu - k matrix
+        IX = xgi.intersection_profile(self.H.H).toarray()
+        #print(IX)
+        IX = np.triu(IX)
+        np.fill_diagonal(IX, 0) 
+        invk = IX[:-1,1:]
+        invk = invk.transpose()
 
-        KVEC = []
-        KPVEC = []
-        NPVEC = []
-        NMUVEC = []
-        BVEC = []
-        NUM_EDGES = []
-
-        for H,e in zip(H_vec, e_vec):
-
-            #facts about the H graph
-            num_of_edges = H.num_edges
-            N = H.num_nodes
-
-            #facts about the added edge
-            k_vec = []
-            kp_vec = []
-            Np_vec = []
-            Nmu_vec = []
-            b_vec = []
-
-            #calculate necessary vectors for EM
-            for i in range(num_of_edges):
-                e_i = H.edges.members(i)
-                N_mu = len(e_i)
-                k = N_mu - len(e.intersection(e_i))
-                Np  = N - N_mu
-                k_prime = len(e.intersection(H.nodes-e_i))
-                new_added = len(e.difference(H.nodes))
-
-
-                k_vec.append(k)
-                kp_vec.append(k_prime)
-                Np_vec.append(Np)
-                Nmu_vec.append(N_mu)
-                b_vec.append(new_added)
-
-
-            if num_of_edges<total_edge:
-                for _ in range(total_edge-num_of_edges):
-                    k_vec.append(0)
-                    kp_vec.append(0)
-                    Np_vec.append(0)
-                    Nmu_vec.append(0)
-                    b_vec.append(new_added)
-            # these are all numpy array matrix, list of lists, row = how many edges added, column = how many edges are in each time
-
-            KVEC.append(k_vec)
-            KPVEC.append(kp_vec)
-            NPVEC.append(Np_vec)
-            NMUVEC.append(Nmu_vec)
-            BVEC.append(b_vec)
-
-            # this is just a list, their length = how many edges added
-            NUM_EDGES.append(num_of_edges)
-
-        #print(KVEC)
-
-        KVEC = np.array(KVEC)
-        KPVEC = np.array(KPVEC)
-        NPVEC = np.array(NPVEC)
-        NMUVEC = np.array(NMUVEC)
-        BVEC = np.array(BVEC)
-        NUM_EDGES = np.array(NUM_EDGES)
+        # If we proprogate this thing and make it an upper triagonal matrix then this is the Nmu
+        # this is necessary because the first hypergraph is omitted if the size of its node set is 0
+        # this is the NMU MATRIX
+        edge_size_vec = self.H.edge_size_sequence()[:-1]
+        Nmu = np.tile(edge_size_vec,(len(edge_size_vec),1)).transpose()
+        Nmu = np.triu(Nmu)
+        NMUVEC = Nmu.transpose()
+        # THIS IS NOT NMU, THIS IS THE LENGTH OF E, THE NEWLY ADDED EDGE
+        new_edge_size = self.H.edge_size_sequence()[1:]
+        NES = np.tile(new_edge_size,(len(new_edge_size),1)).transpose()
+        # KVECS
+        KVEC = NMUVEC-invk
+        # N vecs
+        Np = np.triu(node_count-Nmu)
+        NPVEC = Np.transpose()
+        # bvec
+        b =np.array(node_count[1:] + [self.H.H.num_nodes]) - np.array(node_count)
+        BVEC = np.tile(b,(len(b),1)).transpose()
+        # This is just kprime because kprime is whatever is left of not the new nodes and not the intersection
+        # kprime = new_edge_size - entirely_new_edges - overlap_with_original
+        other_nodes_mat = (NES - BVEC) - invk
+        KPVEC = np.tril(other_nodes_mat)
+        NUM_EDGES = edge_count
 
 
         count = 0
@@ -104,7 +74,7 @@ class EM():
 
 
             PZXVEC, total_vec, bscore_vec = self.E_step(heta, hgamma, hbeta, KVEC, KPVEC, NPVEC, NMUVEC, BVEC, NUM_EDGES)
-            cor= Test_Correct_vec(NUM_EDGES, total_vec, bscore_vec) 
+            cor= self.Test_Correct_vec(NUM_EDGES, total_vec, bscore_vec) 
             heta, hgamma, hbeta = self.M_step(PZXVEC, KVEC, KPVEC, NPVEC, NMUVEC, BVEC, NUM_EDGES) 
 
             count = count+1
@@ -121,7 +91,8 @@ class EM():
         s4 = np.power(hgamma, KPVEC)
 
         pzx = s1*s2*s3*s4
-
+        bscore_vec = (np.power(hbeta, BVEC)*np.exp(hbeta))/(ss.factorial(BVEC))
+    
         pzx_vec= []        
         total_vec=[]
 
@@ -136,11 +107,12 @@ class EM():
         return PZXVEC, total_vec, bscore_vec
 
     def M_step(self, PZXVEC, KVEC, KPVEC, NPVEC, NMUVEC, BVEC, NUM_EDGES):
-
+        
         heta =  1-(sum(np.multiply(KVEC, PZXVEC).sum(1))/sum(np.multiply(NMUVEC, PZXVEC).sum(1)))
         hgamma = sum(np.multiply(KPVEC, PZXVEC).sum(1))/sum(np.multiply(NPVEC, PZXVEC).sum(1))
         hbeta = sum(np.multiply(BVEC, PZXVEC).sum(1))/np.sum(PZXVEC)  
 
+        #print(heta, hgamma, hbeta)
         return heta, hgamma, hbeta
 
 
