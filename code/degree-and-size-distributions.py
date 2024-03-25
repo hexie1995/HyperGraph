1033ebac9e2bdebbe0d320218d16918ba844c3e2
Phil Chodrow <pchodrow@middlebury.edu>
2024-03-25 10:39:14 -0400

    Update degree-and-size-distributions.py

--- code/degree-and-size-distributions.py
+++ code/degree-and-size-distributions.py
@@ -17,7 +17,7 @@ ll = importlib.import_module(".likelihoods", "src")
 
 # +
 plt.style.use('seaborn-v0_8-whitegrid')
-os.makedirs("code/figures/moments",exist_ok = True)
+os.makedirs("figures/moments",exist_ok = True)
 
 
 realworld_Hgraphs = ["coauth-dblp", "coauth-mag-geology", "coauth-mag-history",
@@ -44,11 +44,11 @@ def read_pars(path = "figures/parameters.csv"):
     return pd.read_csv(path).set_index("dataset")
 
 def prepare_figure(data_names):
-    fig, ax = plt.subplots(2, len(data_names), figsize = (3.3*len(data_names), 6))
-    for i, data_name in enumerate(data_names):
+    fig, ax = plt.subplots(3, len(data_names), figsize = (3.3*len(data_names), 9))
+    # for i, data_name in enumerate(data_names):
         # par = pars[data_name]
         # eta, beta, gamma = par["eta"], par["beta"], par["gamma"]
-        ax[0,i].set(title = data_name)
+        # ax[0,i].set(title = data_name)
         # fig.suptitle(fr"{data_name}:$\eta = ${eta:.3f}, $\beta = ${beta:.3f},$\gamma = ${gamma:.3f}")
     return fig, ax
 
@@ -73,7 +73,7 @@ def degree_histogram(H, par, ax, label_y = True):
     hist, bins = np.histogram(d, bins = min(int(len(d)/5), 100))
     p = hist/hist.sum()
 
-    ax.scatter(bins[:-1], p,  facecolors='none', edgecolors =  'cornflowerblue')
+    ax.scatter(bins[:-1], p,  facecolors='none', edgecolors =  'cornflowerblue', linewidth = 2)
     ax.loglog()
     
     eta, beta, gamma = par["eta"], par["beta"], par["gamma"]
@@ -101,21 +101,25 @@ def degree_histogram(H, par, ax, label_y = True):
     ax.plot(x, y, linestyle = "--", color = "black", zorder = -10, linewidth=1)
 
     ax.annotate(fr"$\zeta=${exponent:.2f}", xy = (x[0]*2, y[0]/2))
-    
+    ax.set(title = "Degree distribution")
 
-def edge_size_histogram(H, par, ax, k_max = 50, show_analytic = False):    
+def edge_size_histogram(H, par, ax, k_max = 50, show_analytic = False, legend = False):    
     k = H.edge_size_sequence()
 
     hist, bins = np.histogram(k, bins = min(int(len(k)/10), 50))
     p = hist / hist.sum()
     ax.semilogy()
-    ax.scatter(bins[:-1], p, facecolors='none', edgecolors =  "sandybrown")
+    ax.scatter(bins[:-1], p, facecolors='none', edgecolors =  "sandybrown", label = "Data", linewidth = 2)
     ax.set(xlabel = "Edge Size", ylabel = None)
     v = asymptotic_edge_size_distribution(par, k_max = k_max)
-    ax.plot(np.arange(1, len(v)+1), v, color = "black", zorder = -10, linestyle = "--", linewidth=1)
+    # ax.plot(np.arange(1, len(v)+1), v, color = "black", zorder = -10, linestyle = "--", linewidth=1)
+    ax.scatter(np.arange(1, len(v)+1), v, c =  "black", zorder = 10, s = 5, label = "Modeled")
+    
     ax.set(ylim = (p[p>0].min()/10, None), xlim = (1, k.max() + 2))
 
     eta, beta, gamma = par["eta"], par["beta"], par["gamma"]
+    
+    
     
     analytic_mean_edge_size = 1 + (gamma + beta) / (1 - eta)
     modeled_mean_edge_size  = (v*np.arange(1, len(v)+1)).sum()
@@ -125,20 +129,55 @@ def edge_size_histogram(H, par, ax, k_max = 50, show_analytic = False):
     if show_analytic: 
         title += f"\nAnalytic mean edge size: {analytic_mean_edge_size:.2f}"
     
+    if legend: 
+        ax.legend()
+    
     ax.set(title = title)
+
+
+def filter_not_updated(v):
+    V = v.copy()
+    D = np.abs(v[:, None] - v[None, :])
+    np.fill_diagonal(D, 1)
+    ix = np.argwhere(D == 0)
+    dupes = set(np.argwhere(np.isclose(D, 0)).ravel())
+    for dupe in dupes: 
+        V[dupe] = np.nan 
+    return V
+
+def parameter_viz(par, ax, k_max = 50, legend = False, data_name = ""):
+    
+    # BETA = par["BETA"]
+    # GAMMA = par["GAMMA"]
+    BETA = filter_not_updated(par["BETA"])
+    GAMMA = filter_not_updated(par["GAMMA"])
+    
+    ax.scatter(np.arange(len(BETA)), BETA, label = r"$\beta$ (novel)", facecolors='none', edgecolors = '#DB7093', linewidth = 2)
+    ax.scatter(np.arange(len(GAMMA)), GAMMA, facecolors='none', edgecolors = '#6B8E23', label = r"$\gamma$ (from hypergraph)", marker = "s", linewidth = 2)
+    ax.semilogy()
+    if legend:
+        ax.legend()
+    
+    title = fr"{data_name}: $\eta = {par['eta']:.2f}$"
+    ax.set(xlabel = "Number of added nodes", ylabel = "Probability", title = title)
     
 
-def make_fig(data_names, fname = "multi"):
+
+def make_fig(data_names, fname = "multi", show_analytic = False):
     pars = {}
     for data_name in data_names:
     #for data_name in ["email-enron"]:
         pars[data_name] = {}
-        with open('sem_results_new/res_{}.pkl'.format(data_name + "_SEM_new"), "rb") as f:
+        with open('code/sem_results_new/res_{}.pkl'.format(data_name + "_SEM_new"), "rb") as f:
             d = pickle.load(f)
 
         pars[data_name]["eta"] = np.mean(d["eta"][-50:])
         pars[data_name]["beta"] = expectation(np.mean(d["beta"][-50:], axis=0))   
         pars[data_name]["gamma"] = expectation(np.mean(d["gamma"][-50:], axis=0))
+        pars[data_name]["BETA"] = np.mean(d["beta"][-50:], axis=0)
+        pars[data_name]["GAMMA"] = np.mean(d["gamma"][-50:], axis=0)
+        
+        
         try:
             pars[data_name]["gamma_vec"] = np.mean(d["gamma"][-50:], axis=0)[0:50]
         except:
@@ -162,19 +201,21 @@ def make_fig(data_names, fname = "multi"):
         H, k_max = prepare_data(data_name)
         par = pars[data_name]
 
-        degree_histogram(H, par,ax[0, i], label_y = (i == 0))
+        parameter_viz(par, ax[0, i], k_max = k_max, legend = i == 0, data_name=data_name)
+        
+        degree_histogram(H, par,ax[1, i], label_y = (i == 0))
         
         # k_max = 100 if data_name == "congress-bills" else 50
         
-        edge_size_histogram(H, par,ax[1, i], k_max = k_max)
+        edge_size_histogram(H, par, ax[2, i], k_max = k_max, show_analytic=show_analytic, legend = i == 0)
         
-        y_min_upper, y_max_upper = ax[0,i].get_ylim()
-        y_min_lower, y_max_lower = ax[1,i].get_ylim()
+        # y_min_upper, y_max_upper = ax[0,i].get_ylim()
+        # y_min_lower, y_max_lower = ax[1,i].get_ylim()
         
-        all_y_min_upper = min(all_y_min_upper, y_min_upper)
-        all_y_min_lower = min(all_y_min_lower, y_min_lower)
-        all_y_max_upper = max(all_y_max_upper, y_max_upper)
-        all_y_max_lower = max(all_y_max_lower, y_max_lower)
+        # all_y_min_upper = min(all_y_min_upper, y_min_upper)
+        # all_y_min_lower = min(all_y_min_lower, y_min_lower)
+        # all_y_max_upper = max(all_y_max_upper, y_max_upper)
+        # all_y_max_lower = max(all_y_max_lower, y_max_lower)
         
     # for i in range(len(data_names)):
     #     ax[0,i].set(ylim = (all_y_min_upper, all_y_max_upper))
@@ -247,11 +288,13 @@ def asymptotic_edge_size_distribution(*args, **kwargs):
 
 make_fig([
             "email-enron",
+            # "email-enron",
             "ndc-classes", 
-            "congress-bills", 
+            "coauth-mag-history", 
             "tags-ask-ubuntu"
         ], 
-        fname = "multi-distribution-fig")
+        fname = "multi-distribution-fig", 
+        show_analytic=False)
 
     
 # make_fig("email-enron")
