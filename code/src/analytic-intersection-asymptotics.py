b7e77000658b0b3f0660a78142a74a5f27a4b502
Philip Chodrow <pchodrow@gmail.com>
2024-05-22 15:10:59 -0400

    some sparsity investigations...

--- code/src/analytic-intersection-asymptotics.py
+++ code/src/analytic-intersection-asymptotics.py
@@ -2,7 +2,6 @@ import numpy as np
 from itertools import product 
 from scipy.special import binom
 
-
 class MatrixConstructor: 
     
     def __init__(self, k_max, pars):
@@ -16,18 +15,13 @@ class MatrixConstructor:
     def a_array(self):
         """
         probably a pretty inefficient implementation
+        attempts to implement alpha from the writeup
         """
         
         k_max = self.k_max
-        eta = self.pars["eta"]
-        BETA = self.pars["beta"]
+        eta   = self.pars["eta"]
+        BETA  = self.pars["beta"]
         GAMMA = self.pars["gamma"]
-        
-        self.A = 0.00*np.ones((k_max, 
-                           k_max, 
-                           k_max, 
-                           k_max, 
-                           k_max))
         
         I = np.arange(k_max)[:, None, None, None, None, None]
         J = np.arange(k_max)[None, :, None, None, None, None]
@@ -41,31 +35,38 @@ class MatrixConstructor:
         for i, y in product(range(k_max), range(k_max)):
             S1_iy[i, y] = sum(BETA[x]*GAMMA[i - y - x] for x in range(0, i - y))
         
-        # reshape 
+        # reshape and tile along missing axes
         S1_iy = S1_iy[:, None, None, None, None, :]
         S1_iy = np.tile(S1_iy, (1, k_max, k_max, k_max, k_max, 1)) 
 
-        # create mask and reshape
+        # create mask to zero out elements where |e \cap f| > |e|. 
         mask = Y > I 
         mask = np.tile(mask, (1, k_max, k_max, k_max, k_max, 1))
         S1_iy[mask] = 0
         
-        print(np.isnan(S1_iy).mean())
+        # just for testing 
+        sparsity = (S1_iy == 0).mean()
+        nan = np.isnan(S1_iy).mean()
+        
+        print("A components")
+        print(f"{sparsity = :.4f}, {nan = :.4f}")
+        
+        
         
         # second term (might not need reshaping, but check)
         S2_kyljh = binom(H, K)*binom(L - H, Y - K)/binom(L, Y)
         # S2_kyljh = S2_kyljh[None, None, :, :, :, :]
         S2_kyljh = np.tile(S2_kyljh, (k_max, k_max, 1, 1, 1, 1))
         
+        # zero out elements where the binomial coefficients are invalid
         mask = (K > H) | (Y > L) | ( Y - K > L - H) | (H > L) | (L == 0) 
         mask = np.tile(mask, (k_max, k_max, 1, 1, 1, 1))
-        # print(mask.shape)
-        # print(S2_kyljh.shape)
-        # print(mask.shape)
-        # need to reshape
         S2_kyljh[mask] = 0
         
-        print(np.isnan(S2_kyljh).mean())
+        # just for testing
+        sparsity = (S2_kyljh == 0).mean()
+        nan = np.isnan(S2_kyljh).mean()
+        print(f"{sparsity = :.4f}, {nan = :.4f}")
         
         # third term
         S3_yljh = Y/L * binom(L-1, Y-1) * eta**(Y-1) * (1-eta)**(L-Y)
@@ -75,18 +76,23 @@ class MatrixConstructor:
         # need to do some more mask engineering on this term too
         mask = (Y > J) | (Y > L) | (L == 0)
         mask = np.tile(mask, (k_max, 1, k_max, 1, k_max, 1))
-        # print(S3_yljh.shape)
-        # print(mask.shape)
-        # need to reshape
-        
         S3_yljh[mask] = 0
         
-        print(np.isnan(S3_yljh).mean())
+        # just for testing
+        sparsity = (S3_yljh == 0).mean()
+        nan = np.isnan(S3_yljh).mean()
+        print(f"{sparsity = :.4f}, {nan = :.4f}")
         
         # marginalize over y
         self.A = (S1_iy*S2_kyljh*S3_yljh).sum(axis = 5)
-        # print(self.A.shape)
-          
+        # self.A = 1 + np.zeros_like(self.A) # just for testing
+        
+        # just for testing
+        print("A")
+        sparsity = (self.A == 0).mean()
+        nan = np.isnan(self.A).mean()
+        print(f"{sparsity = :.4f}, {nan = :.4f}")
+        
     def b_array(self): 
         """
         array of coefficients for first term in the linear map
@@ -106,16 +112,22 @@ class MatrixConstructor:
         J = np.arange(self.k_max)[None, :, None]
         K = np.arange(self.k_max)[None, None, :]
         
-        M = binom(J-1, K-1)*eta**(K-1)*(1-eta)**(J-K) 
-        M[np.isnan(M)] = 0
-        
+        M = K/J * binom(J-1, K-1)*eta**(K-1)*(1-eta)**(J-K) 
+        mask = (K > J) | (J == 0)
+        M[mask] = 0
+                   
         for i, j, k in product(range(self.k_max), range(self.k_max), range(self.k_max)):
             
             self.B[i,j,k] = M[:,j,k] * sum(
                 BETA[ell]*GAMMA[i-k-ell]
                 for ell in range(1, i - k)
             )
-            
+        
+        print("B")
+        sparsity = (self.B == 0).mean()
+        nan = np.isnan(self.B).mean()
+        
+        print(f"{sparsity = :.4f}, {nan = :.4f}")
         
     def linear_map(self, T): 
         self.k_max = T.shape[0]
@@ -126,23 +138,25 @@ class MatrixConstructor:
             
             # Case 1: k = 0
             if k == 0: 
-                S[i,j,0] = 1/2*sum(
+                S[i,j,k] = 1/2*sum(
                                   T[ell,j,0]*self.A[i,0,ell,j,0] + 
                                   T[j,ell,0]*self.A[i,0,j,ell,0]
                                   for ell in range(self.k_max)
                                 )
+                # S[i,j,k] = 1 
             
             # Case 2: k >= 1
             else: 
                 S[i,j,k] = 1/2*sum(
                                 T[ell,j,k]*self.A[i,k,ell,j,k] + 
                                 T[j,ell,k]*self.A[i,k,j,ell,k]
-                                for ell, h in product(range(self.k_max), range(self.k_max)) if h >= k
+                                for ell, h in product(range(self.k_max), range(self.k_max))
                                 )
                 
                 S[i,j,k] += self.B[i, j, k]*(sum(
                     T[ell,j,k] + T[j,ell,k] for ell in range(self.k_max)
                 ))
+                # S[i, j, k] += 1
                     
         return S
     
